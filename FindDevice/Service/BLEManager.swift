e99bd7c6a6e294f5f97ea74c3b97d455fc229051
Vyacheslav <financce@gmail.com>
2021-12-07 14:07:09 +0300

    added BLEManager

---
+++ FindDevice/Service/BLEManager.swift
@@ -0,0 +1,13 @@
+//
+//  BLEManager.swift
+//  FindDevice
+//
+//  Created by slava on 07/12/2021.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+
+}
