f23637514a1fd8b03ec147dcaf056b63dbf0f14f
Vyacheslav <financce@gmail.com>
2021-12-07 14:10:44 +0300

    added status if bluethos on

--- FindDevice/Service/BLEManager.swift
+++ FindDevice/Service/BLEManager.swift
@@ -9,5 +9,25 @@ import Foundation
 import CoreBluetooth
 
 class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+    //MARK: Property
+    @Published var isSwitchedOn = false
+    
+    var myCentral: CBCentralManager!
+    
+    override init() {
+           super.init()
 
+           myCentral = CBCentralManager(delegate: self, queue: nil)
+           myCentral.delegate = self
+       }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            isSwitchedOn = true
+                
+        } else {
+            isSwitchedOn = false
+        }
+    }
+    
 }
